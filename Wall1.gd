1621093906ef350a1641a86874b7a720334508fb
geithamum <136993352+geithamum@users.noreply.github.com>
2024-02-18 04:54:58 -0700

    Added level template with shrinking walls

--- Wall1.gd
+++ Wall1.gd
@@ -1,14 +1,13 @@
 extends CharacterBody2D
 var direction = Vector2.ZERO
 
-const SPEED = 0.25
+
+const SPEED = 0.1
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-func _physics_process(delta):
-	direction = Vector2(0, -1)
-	velocity = SPEED * direction
-	move_and_slide()
+func _moveWallUp():
+	position.y -= 10
